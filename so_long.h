6732340053c1959f99bf431ffb099ce6c5747ba8
rnittabadcommit <ryushironitta@yahoo.co.jp>
2022-06-04 20:12:02 +0200

    modified to compile

--- so_long.h
+++ so_long.h
@@ -7,7 +7,38 @@
 
 #include "./ft_malloc/ft_malloc.h"
 
-typedef t_err *char
+#define MAP_SIZE_MAX 1 << 30
+typedef char **map;
+typedef struct s_me
+{
+	int y;
+	int x;
+} t_me;
 
+typedef enum
+{
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	NO_MOVE,
+} e_dir;
+
+typedef enum
+{
+	TOO_LARGE_MAP,
+	NOT_RECTANGLE,
+	INVALID_CHAR,
+	INVALID_CHAR_NUM,
+	NOT_CLOSED_MAP,
+	NO_ERR,
+} e_err;
+
+typedef struct s_all
+{
+	t_me me;
+	char **map;
+	e_err err;
+} t_all;
 
 #endif
