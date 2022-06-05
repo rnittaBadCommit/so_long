5de7cd0c409b28e52793cd14aa6ab9432d0ce58c
rnittabadcommit <ryushironitta@yahoo.co.jp>
2022-06-05 19:37:26 +0200

    modify

--- utils/util1.c
+++ utils/util1.c
@@ -1,11 +1,13 @@
+#include "../so_long.h"
+
 void zero_reset(void *a, void *b,void *c, void *d)
 {
 	if (a != NULL)
-		*a = 0;
+		*(int *)a = 0;
 	if (b != NULL)
-		*b = 0;
+		*(int *)b = 0;
 	if (c != NULL)
-		*c = 0;
+		*(int *)c = 0;
 	if (d != NULL)
-		*d = 0;
+		*(int *)d = 0;
 }
