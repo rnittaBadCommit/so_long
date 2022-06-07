f9935cd90f8b51ac6d296b53e409ee16c03c4f1c
rnitta <ryushironitta@yahoo.co.jp>
2022-06-08 00:46:38 +0900

    modify screen_main

--- screen/screen_main.c
+++ screen/screen_main.c
@@ -1,19 +1,30 @@
 #include "../so_long.h"
 
-int		re_paste_hook(t_display	display)
+int	re_paste(t_display	display)
 {
 	mlx_put_image_to_window(display.mlx, display.mlx_win,
 		display.front_img.img, 0, 0);
 	return (0);
 }
 
+int	handle_key(int key, t_all *all)
+{
+	if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN)
+		move_main(all, key);
+	else if (key == KEY_ESC)
+		ft_exit(all);
+	return (0);
+}
+
 void	screen_main(t_all all, t_display display)
 {
+	all.display.mlx = mlx_init();
+	all.display.mlx_win = mlx_new_window(all.display.mlx, all.display.resolution.x, all.display.resolution.y, all.display.name);
 	mlx_clear_window(display.mlx, display.mlx_win);
-	make_screen(&all);
+	set_image(all.map, all.display);
 	mlx_put_image_to_window(display.mlx, display.mlx_win, display.front_img.img, 0, 0);
-	mlx_hook(display.mlx_win, 2, 1L << 0, handle_key, &all);
-	mlx_hook(display.mlx_win, 17, 1L << 17, ft_exit, &all);
-	mlx_hook(display.mlx_win, 9, 1L << 21, re_paste_hook, &all);
+	mlx_hook(display.mlx_win, KEY_PRESS, MASK_KEY_PRESS, handle_key, &all);
+	mlx_hook(display.mlx_win, SCREEN_DESTROY, MASK_STRUCTURE_NOTIFY, ft_exit, &all);
+	mlx_hook(display.mlx_win, FOCUS_IN, MASK_FOCUS_CHANGE, re_paste, &all);
 	mlx_loop(display.mlx);
 }
